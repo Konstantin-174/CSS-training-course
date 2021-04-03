7d4ee40690fdc89d5deea3ff792e1087d765e800
Konstantin-174 <timokhin_74@hotmail.com>
2021-04-03 18:20:17 +0500

    Rebuild project on React.JS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Header from './components/header/Header';
 
 function App() {
     return (
-        <div className="App">
+        <div className="">
             <Header/>
         </div>
     );

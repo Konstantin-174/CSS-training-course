6386c10a20d66849ef9b50649e1908ba89d2c2ce
Konstantin-174 <timokhin_74@hotmail.com>
2021-04-03 20:17:26 +0500

    Rebuild project on React.JS

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -4,8 +4,8 @@ import s from './Stats.module.scss'
 
 const Stats = () => {
     return (
-        <section className={classes.sectionOuter}>
-            <div className={`${classes.sectionInner} + ${s.wrapper}`}>
+        <section className={`${classes.sectionOuter} + ${s.wrapper}`}>
+            <div className={`${classes.sectionInner} + ${s.inner}`}>
                 STATS
             </div>
         </section>

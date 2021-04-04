b7c1b285e4f24be9880eb1e33607a46cab7f18f8
Konstantin-174 <timokhin_74@hotmail.com>
2021-04-04 19:36:59 +0500

    Rebuild project on React.JS

--- src/components/stats/Stats.jsx
+++ src/components/stats/Stats.jsx
@@ -1,12 +1,29 @@
 import React from 'react';
 import classes from '../../styles/Common.module.scss'
 import s from './Stats.module.scss'
+import iconR from './icons/right.svg'
+import iconL from './icons/left.svg'
 
 const Stats = () => {
     return (
         <section className={`${classes.sectionOuter} + ${s.wrapper}`}>
-            <div className={`${classes.sectionInner} + ${s.inner}`}>
-                STATS
+            <div className={s.left}>
+                <div className={s.icon}>
+                    <img src={iconL} alt="Left icon"/>
+                </div>
+                <div className={s.number}>89%</div>
+                <div className={s.text}>
+                    Customers who have increased their productivity
+                </div>
+            </div>
+            <div className={s.right}>
+                <div className={s.icon}>
+                    <img src={iconR} alt="Right icon"/>
+                </div>
+                <div className={s.number}>3123</div>
+                <div className={s.text}>
+                    Users who have used our application
+                </div>
             </div>
         </section>
     )

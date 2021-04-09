a268f7699f237ee83e8afb3dcf7006760a51ee28
Konstantin-174 <timokhin_74@hotmail.com>
2021-04-09 23:22:35 +0500

    Rebuild project on React.JS

--- src/components/newsLetter/NewsLetter.jsx
+++ src/components/newsLetter/NewsLetter.jsx
@@ -5,8 +5,25 @@ import s from './NewsLetter.module.scss';
 const NewsLetter = () => {
     return (
         <section className={`${classes.sectionOuter} + ${s.wrapper}`}>
-            <div className={classes.sectionInner}>
-                NEWSLETTER
+            <div className={`${classes.sectionInner} + ${s.inner}`}>
+                <div className={s.left}>
+                    <div className={s.title}>
+                        Sign up for newsletter
+                    </div>
+                    <div className={s.text}>
+                        Cu qui soleat partiendo urbanitas. Eum aperiri indoctum eu, homero alterum.
+                    </div>
+                </div>
+                <div className={s.right}>
+                    <div className={classes.btnGroup}>
+                        <a href="#" className={`${classes.btn} + ${classes.secondaryBtn}`}>
+                            Email address
+                        </a>
+                        <a href="#" className={`${classes.btn} + ${classes.noBgBtn}`}>
+                            Save me
+                        </a>
+                    </div>
+                </div>
             </div>
         </section>
     )
